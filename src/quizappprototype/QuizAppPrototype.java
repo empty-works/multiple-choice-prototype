94dc0ea1f924f09474c6fb13bfac10b9a20b2d7c
empty-works <pama.mark@gmail.com>
2019-11-25 08:36:45 +0100

    Revert "Initial build."
    This reverts commit f337d4d9bbcbe20e7d7b3f15a8c52bdd9312b235.

--- src/quizappprototype/QuizAppPrototype.java
+++ src/quizappprototype/QuizAppPrototype.java
@@ -2,8 +2,6 @@
  */
 package quizappprototype;
 
-import quizappprototype.user_interface.MainFrame;
-
 /**
  *
  * @author MP
@@ -14,9 +12,7 @@ public class QuizAppPrototype {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
-        MainFrame mf = new MainFrame();
-        mf.setVisible(true);
+        // TODO code application logic here
     }
     
 }
