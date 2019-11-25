b77133f5eec9ab929c55e77e339c58635b0a2ee3
empty-works <pama.mark@gmail.com>
2019-11-25 08:14:00 +0100

    Initial build.

---
+++ src/quizappprototype/QuizAppPrototype.java
@@ -0,0 +1,18 @@
+/*
+ */
+package quizappprototype;
+
+/**
+ *
+ * @author MP
+ */
+public class QuizAppPrototype {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
