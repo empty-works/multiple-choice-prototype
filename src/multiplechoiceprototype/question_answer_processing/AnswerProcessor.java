e84377a4b068f8b70e4dc3a85790509b500a6f00
empty-works <pama.mark@gmail.com>
2019-11-27 09:46:39 +0100

    Created the AnswerProcessor class which will handle generating randomized multiple choice answers.

---
+++ src/multiplechoiceprototype/question_answer_processing/AnswerProcessor.java
@@ -0,0 +1,14 @@
+/*
+ * This class will handle generating randomized answers. In the case of a 
+ * programming language answer or math answer, the algorithms will generate
+ * a pretty good number varying answers.
+ */
+package multiplechoiceprototype.question_answer_processing;
+
+/**
+ *
+ * @author MP
+ */
+public class AnswerProcessor {
+    
+}
