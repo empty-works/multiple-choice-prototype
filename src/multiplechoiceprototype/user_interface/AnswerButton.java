1a9ac86c4ee5b1440a76f6747f36196bd97165eb
empty-works <pama.mark@gmail.com>
2019-11-30 06:44:42 +0100

    Set some properties for the JLabel in the AnswerButton class. Set up a flag for the correct answer, and set the incoming answer as a local variable.

--- src/multiplechoiceprototype/user_interface/AnswerButton.java
+++ src/multiplechoiceprototype/user_interface/AnswerButton.java
@@ -8,13 +8,16 @@ package multiplechoiceprototype.user_interface;
  */
 public class AnswerButton extends javax.swing.JPanel {
 
+    private boolean isCorrectAnswer = false;
+    private String text = "";
+    
     /**
      * 
      */
-    public AnswerButton() {
+    public AnswerButton(String text) {
         initComponents();
         
-        
+        this.text = text;
     }
     
     
@@ -22,19 +25,25 @@ public class AnswerButton extends javax.swing.JPanel {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
-        jLabel1 = new javax.swing.JLabel();
+        ButtonLabel = new javax.swing.JLabel();
 
         setLayout(new java.awt.GridBagLayout());
 
-        jLabel1.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
-        jLabel1.setText("jLabel1");
-        jLabel1.setToolTipText(null);
-        add(jLabel1, new java.awt.GridBagConstraints());
+        ButtonLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
+        ButtonLabel.setText("jLabel1");
+        ButtonLabel.setToolTipText(null);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.weighty = 0.1;
+        gridBagConstraints.insets = new java.awt.Insets(5, 5, 5, 5);
+        add(ButtonLabel, gridBagConstraints);
     }// </editor-fold>//GEN-END:initComponents
 
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
-    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel ButtonLabel;
     // End of variables declaration//GEN-END:variables
 }
