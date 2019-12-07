782b095880d6b02c9ef341c6184998faabc7a702
empty-works <pama.mark@gmail.com>
2019-12-07 20:12:47 +0100

    The reason button is now properly sized to match the proportions of the continue button.

--- src/multiplechoiceprototype/user_interface/ReasonButton.java
+++ src/multiplechoiceprototype/user_interface/ReasonButton.java
@@ -30,16 +30,19 @@ public class ReasonButton extends javax.swing.JPanel {
     @SuppressWarnings("unchecked")
     // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
     private void initComponents() {
+        java.awt.GridBagConstraints gridBagConstraints;
 
         MainLabel = new javax.swing.JLabel();
 
         setToolTipText(null);
-        setLayout(new java.awt.GridLayout());
+        setOpaque(false);
+        setLayout(new java.awt.GridBagLayout());
 
         MainLabel.setFont(new java.awt.Font("Arial", 1, 12)); // NOI18N
         MainLabel.setHorizontalAlignment(javax.swing.SwingConstants.CENTER);
         MainLabel.setText("Why?");
         MainLabel.setToolTipText(null);
+        MainLabel.setOpaque(true);
         MainLabel.addMouseListener(new java.awt.event.MouseAdapter() {
             public void mouseEntered(java.awt.event.MouseEvent evt) {
                 MainLabelMouseEntered(evt);
@@ -51,7 +54,12 @@ public class ReasonButton extends javax.swing.JPanel {
                 MainLabelMousePressed(evt);
             }
         });
-        add(MainLabel);
+        gridBagConstraints = new java.awt.GridBagConstraints();
+        gridBagConstraints.fill = java.awt.GridBagConstraints.BOTH;
+        gridBagConstraints.weightx = 0.1;
+        gridBagConstraints.weighty = 0.1;
+        gridBagConstraints.insets = new java.awt.Insets(5, 0, 10, 10);
+        add(MainLabel, gridBagConstraints);
     }// </editor-fold>//GEN-END:initComponents
 
     private void MainLabelMouseEntered(java.awt.event.MouseEvent evt) {//GEN-FIRST:event_MainLabelMouseEntered
