5e2ec38fd968efad5f3f5287583d1c91cb42cf95
empty-works <pama.mark@gmail.com>
2020-02-24 18:29:12 +0100

    Converting to JavaFX.

---
+++ src/multiplechoiceprototype/user_interface/MultipleChoicePrototype.java
@@ -0,0 +1,27 @@
+/*
+ */
+package multiplechoiceprototype.user_interface;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+/**
+ *
+ * @author MP
+ */
+public class MultipleChoicePrototype extends Application {
+
+    private Scene scene;
+    
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        
+        // Set root.
+        
+        
+        // Set scene.
+        
+        // Set stage.
+    }
+}
