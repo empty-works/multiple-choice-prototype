1245c660bae4b605e23e72d66c2d381a956e5c30
empty-works <pama.mark@gmail.com>
2020-02-24 18:43:17 +0100

    Set properties for the primary stage.

--- src/multiplechoiceprototype/user_interface/MultipleChoicePrototype.java
+++ src/multiplechoiceprototype/user_interface/MultipleChoicePrototype.java
@@ -5,6 +5,7 @@ package multiplechoiceprototype.user_interface;
 import javafx.application.Application;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 /**
  *
@@ -22,6 +23,24 @@ public class MultipleChoicePrototype extends Application {
         
         // Set scene.
         
+        
         // Set stage.
+        setPrimaryStage(primaryStage);
+    }
+    
+    private void setPrimaryStage(Stage primaryStage) {
+        
+        primaryStage.setTitle("Multiple Choice Prototype");
+        primaryStage.initStyle(StageStyle.UNIFIED);
+        primaryStage.setMinWidth(700);
+        primaryStage.setWidth(700);
+        primaryStage.setHeight(800);
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+    
+    public static void main(String[] args) {
+        
+        launch(args);
     }
 }
