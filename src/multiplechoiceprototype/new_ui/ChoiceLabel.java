34ece9b749dfe65402af5a82e0da19fbb540fdfc
empty-works <pama.mark@gmail.com>
2020-02-26 23:46:58 +0100

    Set some ChoiceLabel properties.

--- src/multiplechoiceprototype/new_ui/ChoiceLabel.java
+++ src/multiplechoiceprototype/new_ui/ChoiceLabel.java
@@ -2,6 +2,9 @@
  */
 package multiplechoiceprototype.new_ui;
 
+import javafx.geometry.Insets;
+import javafx.scene.layout.AnchorPane;
+
 /**
  *
  * @author MP
@@ -16,5 +19,13 @@ public class ChoiceLabel extends javafx.scene.control.Label {
         this.setId("ChoiceLabel");
         this.text = text;
         this.setText(text);
+        setProperties();
+    }
+    
+    private void setProperties() {
+        
+        this.setPadding(new Insets(10));
+        AnchorPane.setLeftAnchor(this, 10.0);
+        AnchorPane.setRightAnchor(this, 10.0);
     }
 }
