7b4bf147a811e456ba8d32cc6e1fa18321672566
empty-works <pama.mark@gmail.com>
2020-03-02 22:22:00 +0100

    CurrencyAPIController is implements Initializable. Created a URLConnection object and created a connection() method.

--- src/multiplechoiceprototype/controllers/CurrencyAPIController.java
+++ src/multiplechoiceprototype/controllers/CurrencyAPIController.java
@@ -2,10 +2,32 @@
  */
 package multiplechoiceprototype.controllers;
 
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
 /**
  *
  * @author MP
  */
-public class CurrencyAPIController {
+public class CurrencyAPIController implements Initializable {
     
+    private URLConnection urlCon;
+    
+    public CurrencyAPIController() {
+        
+        
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        
+        
+    }
+    
+    public void connection() {
+        
+        
+    }
 }
