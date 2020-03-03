8b1825fae52465eabcd4e93994a542896a08c9b5
empty-works <pama.mark@gmail.com>
2020-03-03 20:28:27 +0100

    Created JsonCurrencyRetrievalTask to connect to a currency exchange API and turn the JSON data into Java objects.

---
+++ src/multiplechoiceprototype/controllers/JsonCurrencyRetrievalTask.java
@@ -0,0 +1,13 @@
+package multiplechoiceprototype.controllers;
+
+import java.util.Collection;
+import javafx.concurrent.Task;
+
+public class JsonCurrencyRetrievalTask<T> extends Task<Collection<T>> {
+
+    @Override
+    protected Collection<T> call() throws Exception {
+        
+        
+    }
+}
